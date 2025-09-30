b5213a38839839e5ac409d6fc35594f03d533479
Adam Křivánek <adamkrivanek@gmail.com>
2025-09-30 19:00:38 +0200

    Update ImageSize.php

--- src/Enums/ImageSize.php
+++ src/Enums/ImageSize.php
@@ -4,20 +4,26 @@ namespace Rasasak\PexelsPicker\Enums;
 
 enum ImageSize
 {
-    case Raw;
-    case Full;
-    case Regular;
+    case Original;
+    case Large2x;
+    case Large;
+    case Medium;
     case Small;
-    case Thumbnail;
+    case Portrait;
+    case Landscape;
+    case Tiny;
 
     public function getPath(): string
     {
         return match ($this) {
-            self::Raw => 'urls.raw',
-            self::Full => 'urls.full',
-            self::Regular => 'urls.regular',
-            self::Small => 'urls.small',
-            self::Thumbnail => 'urls.thumb',
+            self::Original => 'src.original',
+            self::Large2x => 'src.large2x',
+            self::Large => 'src.large',
+            self::Medium => 'src.medium',
+            self::Small => 'src.smal',
+            self::Portrait => 'src.portrait',
+            self::Landscape => 'src.landscape',
+            self::Tiny => 'src.tiny',
         };
     }
 }
