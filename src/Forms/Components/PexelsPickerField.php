1f5824d392a3614a91c696037f18b1d9aa938e81
Adam Křivánek <adamkrivanek@gmail.com>
2025-09-30 16:38:33 +0200

    Update and rename UnsplashPickerField.php to PexelsPickerField.php

---
+++ src/Forms/Components/PexelsPickerField.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rasasak\PexelsPicker\Forms\Components;
+
+use Filament\Forms\Components\Field;
+
+/**
+ * @internal Internal field component for UnsplashPicker to manage state of selected images.
+ */
+class PexelsPickerField extends Field
+{
+    protected string $view = 'pexels-picker::forms.components.pexels-picker-field';
+}
